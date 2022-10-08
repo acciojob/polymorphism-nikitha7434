084fc2b656617c8f87e9ab8eb2e1ea66d53d04f4
Nikitha <nikithamashetti434@gmail.com>
2022-10-08 21:26:07 +0530

    adding code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+    public void main(String[] args) {
+        Product p=new Product();
+       System.out.print(p.product(2,3));
+        p.product(4,5,6);
+        p.product(5,7);
 
+    }
+
+    class Product {
+
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+
+    }
 }
